65b49d7b03b7060e00bbbac7356e0aeaba63e059
Qiuqiang Kong <qiuqiangkong@gmail.com>
2025-02-06 01:16:50 +0800

    update

---
+++ env.sh
@@ -0,0 +1,9 @@
+pip install numpy==2.2.1
+pip install torch==2.5.1
+pip install tqdm==4.67.1
+pip install wandb==0.19.1
+pip install accelerate==1.2.1
+pip install audidata==0.0.3
+pip install librosa==0.10.2
+pip install descript-audio-codec
+pip install transformers==4.45.2
